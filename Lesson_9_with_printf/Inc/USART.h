f81027fff48abbd478e3077fa9e2d577e27dc9db
Zidane D'souza <zidanestark@gmail.com>
2025-11-20 19:32:27 +0530

    Updated lesson number 9 by using the printf call from stdio.h

---
+++ Lesson_9_with_printf/Inc/USART.h
@@ -0,0 +1,15 @@
+#ifndef __USART_H_
+#define __USART_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f302x8.h"
+
+#define SYSCLK_D       16000000
+#define PERIPH_CLK_D   SYSCLK_D
+#define UART_BAUD_RATE_D 115200
+
+extern void USART_vInit(void);
+extern void USART_vWrite(uint16_t ch);
+
+#endif
